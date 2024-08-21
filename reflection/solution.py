7e5083a28928a041a63cedbd7fdb0c8b7adef1c6
Vincenzo Ciancia <vincenzoml@gmail.com>
2024-08-21 17:07:28 +0000

    WIP

--- reflection/solution.py
+++ reflection/solution.py
@@ -1,13 +1,42 @@
 # %%
 import SimpleITK as sitk
 import os
+import numpy as np
 
-os.makedirs("reflection/output", exist_ok=True)
-image_path = "reflection/sub-stroke0003_ses-01-ct+ax.nii.gz"
-result_path = "reflection/output/result.nii.gz"
+os.makedirs("output", exist_ok=True)
+image_path = "sub-stroke0003_ses-01-ct+ax.nii.gz"
+result_path = "output/result.nii.gz"
 
-image = sitk.ReadImage(image_path)
+input = sitk.ReadImage(image_path)
+np_output = np.array(sitk.GetArrayViewFromImage(input))
 
-image2 = sitk.Threshold(image, 0, 10)
+z, y, x = np.mgrid[:np_output.shape[0],
+                   :np_output.shape[1], :np_output.shape[2]]
 
-sitk.WriteImage(image2, result_path)
+a, b, c, d = [0.9177152418942791, 0.08030779707443489,
+              0.3890364925350529, -290.8034951038973]
+
+
+for index, pixel_value in np.ndenumerate(np_output):
+    # index will contain the (z, y, x) coordinates for a 3D image
+    # pixel_value will be the value at that coordinate
+    cx, cy, cz = index
+    if (a*cx + b*cy + c*cz + d < 1):
+        np_output[index] = 17000
+
+image_output = sitk.GetImageFromArray(np_output)
+image_output.CopyInformation(input)
+
+sitk.WriteImage(image_output, result_path)
+
+# image_x = sitk.GetImageFromArray(x)
+# image_y = sitk.GetImageFromArray(y)
+# image_z = sitk.GetImageFromArray(z)
+#
+# image_x.CopyInformation(input)
+# image_y.CopyInformation(input)
+# image_z.CopyInformation(input)
+#
+# sitk.WriteImage(image_x, "x.nii.gz")
+# sitk.WriteImage(image_y, "y.nii.gz")
+# sitk.WriteImage(image_z, "z.nii.gz")
